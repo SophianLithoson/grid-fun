0f7b96894c824128eb710b22954c0c90978ae228
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-23 12:37:52 -0600

    fixed syntax errors

---
+++ gridfun.js
@@ -0,0 +1,7 @@
+const gridArea = document.querySelector("#grid-area");
+const numSquaresPerSide = 16;
+const squareSize = gridArea.offsetWidth / numSquaresPerSide;
+
+console.log(gridArea);
+console.log(numSquaresPerSide);
+console.log(squareSize);
