86212845d6f32885c006ee6b9983c8740bed5d5b
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-23 14:29:43 -0600

    got squares to display in grid

--- gridfun.js
+++ gridfun.js
@@ -1,7 +1,13 @@
 const gridArea = document.querySelector("#grid-area");
 const numSquaresPerSide = 16;
 const squareSize = gridArea.offsetWidth / numSquaresPerSide;
+let squareColor = "black";
 
-console.log(gridArea);
-console.log(numSquaresPerSide);
-console.log(squareSize);
+for(let i = 0; i < (numSquaresPerSide ** 2); i++) {
+    const squareToAdd = document.createElement("div");
+    squareToAdd.setAttribute("class", "default-box");
+    squareToAdd.style.width = squareSize + "px";
+    squareToAdd.style.height = squareSize + "px";
+    gridArea.appendChild(squareToAdd);
+}
+    
