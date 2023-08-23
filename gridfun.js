cf79c399730b403e24b70dfd11ae5d9676047611
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-23 15:59:08 -0600

    set mouse event behavior

--- gridfun.js
+++ gridfun.js
@@ -8,6 +8,25 @@ for(let i = 0; i < (numSquaresPerSide ** 2); i++) {
     squareToAdd.setAttribute("class", "default-box");
     squareToAdd.style.width = squareSize + "px";
     squareToAdd.style.height = squareSize + "px";
+    squareToAdd.addEventListener("mouseover", divEntered);
+    squareToAdd.addEventListener("mouseleave", divLeft);
+    squareToAdd.addEventListener("mousedown", divClicked);
+
     gridArea.appendChild(squareToAdd);
 }
-    
+
+function divEntered(e) {
+    if (this.hasAttribute("class"))
+        this.setAttribute("class", "hovered-box");
+}
+
+function divClicked() {
+    if (this.hasAttribute("class"))
+        this.removeAttribute("class");
+    this.style.backgroundColor = squareColor;
+}
+
+function divLeft() {
+    if (this.hasAttribute("class"))
+        this.setAttribute("class", "default-box");
+}
