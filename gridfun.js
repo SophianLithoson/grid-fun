8947237fc96c6b9a442ec5c460d6d18fe609f4ea
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-08-23 20:10:14 -0600

    made grid generation into function, added behavior for getting new grid

--- gridfun.js
+++ gridfun.js
@@ -1,28 +1,25 @@
 const gridArea = document.querySelector("#grid-area");
 const resetButton = document.querySelector("#reset-button");
-const numSquaresPerSide = 16;
-const squareSize = gridArea.offsetWidth / numSquaresPerSide;
+const newGridButton = document.querySelector("new-grid-button");
+let numSquaresPerSide = 16;
 let squareColor = "black";
 
+setGrid(numSquaresPerSide);
+
 // set event listeners in this block
-for (let i = 0; i < (numSquaresPerSide ** 2); i++) {
-    const squareToAdd = document.createElement("div");
-    squareToAdd.setAttribute("class", "default-box");
-    squareToAdd.style.width = squareSize + "px";
-    squareToAdd.style.height = squareSize + "px";
-    squareToAdd.addEventListener("mouseover", divEntered);
-    squareToAdd.addEventListener("mouseleave", divLeft);
-    squareToAdd.addEventListener("mousedown", divClicked);
-
-    gridArea.appendChild(squareToAdd);
-}
-
 resetButton.addEventListener("click", () => {
     const squares = document.querySelectorAll("#grid-area div");
     for (let square of squares) {
         square.style.backgroundColor = null;
         square.setAttribute("class", "default-box");
     }
+});
+
+newGridButton.addEventListener("click", () => {
+    do {
+        newNumSquares = prompt("How many squares per side? Must be between 1-100");
+    } while (newNumSquares <= 100 && newNumSquares > 1);
+
 });
 
 // define event functions here
@@ -40,4 +37,25 @@ function divClicked() {
 function divLeft() {
     if (this.hasAttribute("class"))
         this.setAttribute("class", "default-box");
+}
+
+function setGrid(squaresPerSide) {
+    let squareSize = gridArea.offsetWidth / squaresPerSide;
+
+    if(gridArea.hasChildNodes()) {
+        while (gridArea.firstChild)
+            gridArea.removeChild(gridArea.firstChild);
+    }
+
+    for (let i = 0; i < (squaresPerSide ** 2); i++) {
+        const squareToAdd = document.createElement("div");
+        squareToAdd.setAttribute("class", "default-box");
+        squareToAdd.style.width = squareSize + "px";
+        squareToAdd.style.height = squareSize + "px";
+        squareToAdd.addEventListener("mouseover", divEntered);
+        squareToAdd.addEventListener("mouseleave", divLeft);
+        squareToAdd.addEventListener("mousedown", divClicked);
+
+        gridArea.appendChild(squareToAdd);
+    }
 }
